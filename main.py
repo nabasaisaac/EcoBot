17cfc67a6ec439c8eb9063db329921219a879a07
nabasaisaac <nabasaisaac16@gmail.com>
2025-11-12 16:52:48 +0300

    Setting up the environment

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from simple_image_download import simple_image_download
 
 response = simple_image_download.simple_image_download()
 
-keywords = [
-    "dirty plastic bottles",
-    "rubbish bottles",
-    "polluted water bottles",
-    "trash bottles"
-]
+# keywords = [
+#     "dirty plastic bottles",
+#     "rubbish bottles",
+#     "polluted water bottles",
+#     "trash bottles"
+# ]
 
 for kw in keywords:
     response.download(kw, limit=300)
