869d9cd4daca633c0c0094dcff68b5e7816eab27
leticia-lakica <lakicaleticia@gmail.com>
2025-11-19 21:08:12 +0300

    working with yolo initial steps, how to detect objects

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from ultralytics import YOLO
 import cv2
+
+# Load your trained YOLO model
+model = YOLO("yolo11n.pt")
+
+# Open the webcam (0 is usually the default camera)
+cap = cv2.VideoCapture(0)
+
