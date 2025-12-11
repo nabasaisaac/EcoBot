d5cdcf0b2890cd4b8dc4afe25611eecc05be362c
Leticia-Lakica <lakiacleticia@gmail.com>
2025-12-11 13:09:30 +0300

    Move the robot arm using angles computed from inverse kinematics

--- Robot/MasterPi/ArmIK/ArmMoveIK.py
+++ Robot/MasterPi/ArmIK/ArmMoveIK.py
@@ -10,3 +10,32 @@ from ArmIK.InverseKinematics import *
 from ArmIK.Transform import getAngle
 from mpl_toolkits.mplot3d import Axes3D
 from HiwonderSDK.Board import setBusServoPulse,getBusServoPulse, setPWMServoPulse, getPWMServoPulse
+
+
+# Move the robot arm using angles computed from inverse kinematics
+ik = IK('arm')
+# Set link lengths
+l1 = ik.l1
+l4 = ik.l4
+ik.setLinkLength(L1=l1+1.3, L4=l4)
+
+class ArmIK:
+    servo3Range = (500, 2500.0, 0, 180.0) # (pulse width, angle)
+    servo4Range = (500, 2500.0, 0, 180.0)
+    servo5Range = (500, 2500.0, 0, 180.0)
+    servo6Range = (500, 2500.0, 0, 180.0)
+
+    def __init__(self):
+        self.setServoRange()
+
+    def setServoRange(self, servo3_Range=servo3Range, servo4_Range=servo4Range, servo5_Range=servo5Range, servo6_Range=servo6Range):
+        # Support different servo models/ranges
+        self.servo3Range = servo3_Range
+        self.servo4Range = servo4_Range
+        self.servo5Range = servo5_Range
+        self.servo6Range = servo6_Range
+        self.servo3Param = (self.servo3Range[1] - self.servo3Range[0]) / (self.servo3Range[3] - self.servo3Range[2])
+        self.servo4Param = (self.servo4Range[1] - self.servo4Range[0]) / (self.servo4Range[3] - self.servo4Range[2])
+        self.servo5Param = (self.servo5Range[1] - self.servo5Range[0]) / (self.servo5Range[3] - self.servo5Range[2])
+        self.servo6Param = (self.servo6Range[1] - self.servo6Range[0]) / (self.servo6Range[3] - self.servo6Range[2])
+
