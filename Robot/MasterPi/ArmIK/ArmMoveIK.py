97ca23c1640ddec5dce6965bbac4b3bdb798d93c
Leticia-Lakica <lakiacleticia@gmail.com>
2025-12-12 09:13:26 +0300

    Convert IK angles to servo pulse widths

--- Robot/MasterPi/ArmIK/ArmMoveIK.py
+++ Robot/MasterPi/ArmIK/ArmMoveIK.py
@@ -38,4 +38,48 @@ class ArmIK:
         self.servo4Param = (self.servo4Range[1] - self.servo4Range[0]) / (self.servo4Range[3] - self.servo4Range[2])
         self.servo5Param = (self.servo5Range[1] - self.servo5Range[0]) / (self.servo5Range[3] - self.servo5Range[2])
         self.servo6Param = (self.servo6Range[1] - self.servo6Range[0]) / (self.servo6Range[3] - self.servo6Range[2])
+    
+    def transformAngelAdaptArm(self, theta3, theta4, theta5, theta6):
+        # Convert IK angles to servo pulse widths
+        servo3 = int(round(theta3 * self.servo3Param + (self.servo3Range[1] + self.servo3Range[0])/2))
+        if servo3 > self.servo3Range[1] or servo3 < self.servo3Range[0]:
+            logger.info('servo3(%s) out of range (%s, %s)', servo3, self.servo3Range[0], self.servo3Range[1])
+            return False
 
+        servo4 = int(round(theta4 * self.servo4Param + (self.servo4Range[1] + self.servo4Range[0])/2))
+        if servo4 > self.servo4Range[1] or servo4 < self.servo4Range[0]:
+            logger.info('servo4(%s) out of range (%s, %s)', servo4, self.servo4Range[0], self.servo4Range[1])
+            return False
+
+        servo5 = int(round((self.servo5Range[1] + self.servo5Range[0])/2 + (90.0 - theta5) * self.servo5Param)) 
+        if servo5 > ((self.servo5Range[1] + self.servo5Range[0])/2 + 90*self.servo5Param) or servo5 < ((self.servo5Range[1] + self.servo5Range[0])/2 - 90*self.servo5Param):
+            logger.info('servo5(%s) out of range (%s, %s)', servo5, self.servo5Range[0], self.servo5Range[1])
+            return False
+
+        if theta6 < -(self.servo6Range[3] - self.servo6Range[2])/2:
+            servo6 = int(round(((self.servo6Range[3] - self.servo6Range[2])/2 + (90 + (180 + theta6))) * self.servo6Param))
+        else:
+            servo6 = int(round(((self.servo6Range[3] - self.servo6Range[2])/2 - (90 - theta6)) * self.servo6Param)) + self.servo6Range[0]
+        if servo6 > self.servo6Range[1] or servo6 < self.servo6Range[0]:
+            logger.info('servo6(%s) out of range (%s, %s)', servo6, self.servo6Range[0], self.servo6Range[1])
+            return False
+        return {"servo3": servo3, "servo4": servo4, "servo5": servo5, "servo6": servo6}
+
+    def servosMove(self, servos, movetime=None):
+        # Drive servos 3/4/5/6 to target pulses
+        time.sleep(0.02)
+        if movetime is None:
+            max_d = 0
+            for i in  range(0, 4):
+                d = abs(getPWMServoPulse(i + 3) - servos[i])
+                if d > max_d:
+                    max_d = d
+            movetime = int(max_d*1)
+        setPWMServoPulse(3, servos[0], movetime)
+        setPWMServoPulse(4, servos[1], movetime)
+        setPWMServoPulse(5, servos[2], movetime)
+        setPWMServoPulse(6, servos[3], movetime)
+        
+#         setPWMServosPulse(movetime, 4, 3,servos[0], 4,servos[1], 5,servos[2], 6,servos[3])
+
+        return movetime
