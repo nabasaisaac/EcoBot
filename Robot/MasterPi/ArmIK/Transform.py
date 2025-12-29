5c8ea35e0cc5f1e612d2973f8467095dc6929d2d
Leticia-Lakica <lakiacleticia@gmail.com>
2025-12-29 21:08:50 +0300

    Add coordinate mapping utilities to convert image pixels to arm coordinate system

--- Robot/MasterPi/ArmIK/Transform.py
+++ Robot/MasterPi/ArmIK/Transform.py
@@ -16,3 +16,19 @@ param_data = np.load(map_param_path + '.npz')
 # Real-world distance represented by one pixel
 map_param_ = param_data['map_param']
 
+# Numeric mapping: map a value from one range to another.
+def leMap(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+# Convert image pixel coordinates to the arm coordinate system.
+# Example input: (x, y, (width, height)) such as (100, 100, (640, 320)).
+def convertCoordinate(x, y, size):
+    x = leMap(x, 0, size[0], 0, 640)
+    x = x - 320
+    x_ = round(x * map_param_, 2)
+
+    y = leMap(y, 0, size[1], 0, 480)
+    y = 240 - y
+    y_ = round(y * map_param_ + image_center_distance, 2)
+
+    return x_, y_
