bb982a1b18add9cf7f563beb3a215de9c9c5a164
Leticia-Lakica <lakiacleticia@gmail.com>
2026-01-20 17:05:22 +0300

    Added gripper initialization, arm positioning, buzzer control, and RGB LED feedback with color detection startup logic

--- Robot/MasterPi/Functions/Color_Warning.py
+++ Robot/MasterPi/Functions/Color_Warning.py
@@ -53,3 +53,61 @@ def getAreaMaxContour(contours):
                 area_max_contour = c
 
     return area_max_contour, contour_area_max  # (largest contour, area)
+
+
+# Gripper "close" servo pulse (tuned for this robot)
+servo1 = 1500
+
+# Initial arm pose
+def initMove():
+    Board.setPWMServoPulse(1, servo1, 300)
+    AK.setPitchRangeMoving((0, 6, 18), 0,-90, 90, 1500)
+
+
+# Buzzer helper
+def setBuzzer(timer):
+    Board.setBuzzer(0)
+    Board.setBuzzer(1)
+    time.sleep(timer)
+    Board.setBuzzer(0)
+   
+
+# Set the controller RGB LED to match the target color.
+def set_rgb(color):
+    if color == "red":
+        Board.RGB.setPixelColor(0, Board.PixelColor(255, 0, 0))
+        Board.RGB.setPixelColor(1, Board.PixelColor(255, 0, 0))
+        Board.RGB.show()
+    elif color == "green":
+        Board.RGB.setPixelColor(0, Board.PixelColor(0, 255, 0))
+        Board.RGB.setPixelColor(1, Board.PixelColor(0, 255, 0))
+        Board.RGB.show()
+    elif color == "blue":
+        Board.RGB.setPixelColor(0, Board.PixelColor(0, 0, 255))
+        Board.RGB.setPixelColor(1, Board.PixelColor(0, 0, 255))
+        Board.RGB.show()
+    else:
+        Board.RGB.setPixelColor(0, Board.PixelColor(0, 0, 0))
+        Board.RGB.setPixelColor(1, Board.PixelColor(0, 0, 0))
+        Board.RGB.show()
+
+color_list = []
+__isRunning = False
+detect_color = 'None'
+size = (640, 480)
+interval_time = 0
+draw_color = range_rgb["black"]
+
+
+# Called once when the module is initialized
+def init():
+    print("ColorDetect Init")
+    load_config()
+    initMove()
+
+# Called when the mode starts
+def start():
+    global __isRunning
+    __isRunning = True
+    print("ColorDetect Start")
+
