67708ad29dca5294fac53dd5cbe389ce11a6f549
Leticia-Lakica <lakiacleticia@gmail.com>
2026-03-05 12:15:11 +0300

    Added LAB-based image processing loop with noise reduction, real-time visualization, and camera integration for calibration testing

--- Robot/MasterPi/Functions/lab_adjust.py
+++ Robot/MasterPi/Functions/lab_adjust.py
@@ -95,3 +95,55 @@ def stop():
     __isRunning = False
     reset()
     print("lab_adjust Stop")
+
+
+# Called when the mode exits (cleanup)
+def exit():
+    global __isRunning
+    __isRunning = False
+    print("lab_adjust Exit")
+
+def run(img):  
+    img_copy = img.copy()
+    img_h, img_w = img.shape[:2]
+    
+    if not __isRunning or __target_color == ():
+        return img
+    
+    frame_gb = cv2.GaussianBlur(img_copy, (3, 3), 3)   
+    frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)  # Convert frame to LAB color space
+    
+    for i in lab_data:
+        if i in __target_color:
+            frame_mask = cv2.inRange(frame_lab,
+                                         (lab_data[i]['min'][0],
+                                          lab_data[i]['min'][1],
+                                          lab_data[i]['min'][2]),
+                                         (lab_data[i]['max'][0],
+                                          lab_data[i]['max'][1],
+                                          lab_data[i]['max'][2]))  # Mask using LAB min/max
+            eroded = cv2.erode(frame_mask, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)))  # Erode (remove noise)
+            dilated = cv2.dilate(eroded, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))) # Dilate (restore shape)
+            frame_bgr = cv2.cvtColor(dilated, cv2.COLOR_GRAY2BGR)
+            img = frame_bgr
+    return img
+
+if __name__ == '__main__':      
+    init()
+    start()
+    my_camera = Camera.Camera()
+    my_camera.camera_open()
+
+    while True:
+        img = my_camera.frame
+        if img is not None:
+            frame = img.copy()
+            Frame = run(frame)           
+            cv2.imshow('Frame', Frame)
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+        else:
+            time.sleep(0.01)
+    my_camera.camera_close()
+    cv2.destroyAllWindows()
